41d7046347dcd6d55f32606fb68a514324a98a0c
Epicflights <thunderbolt1003_18@orange.fr>
2019-08-02 20:16:12 +0200

    Première version
    Première version du script avec GUI (Tkinter) - améliorations à prévoir

---
+++ SortNM.py
@@ -0,0 +1,51 @@
+from tkinter.filedialog import askopenfilename
+ntmfile = askopenfilename()
+
+def rechercheFree(filename,word):
+    logs = 0
+    with  open(filename) as f:
+        for line in f:
+            if word in line:
+                print(line)
+                charline = line
+                logs += 1
+    return logs
+
+plmnFree = '4G;208;15';
+plmnOR = '4G;208;1;';
+plmnBT = '4G;208;20';
+plmnSFR = '4G;208;10';
+
+fr = rechercheFree(ntmfile,plmnFree)
+OR = rechercheFree(ntmfile,plmnOR)
+bt = rechercheFree(ntmfile,plmnBT)
+sfr = rechercheFree(ntmfile,plmnSFR)
+
+#summary
+print('---------------------------------------------------------------------------------------------------------------')
+print(str(fr)+" Instances de "+str(plmnFree)+" trouvées dans "+str(ntmfile))
+print(str(OR)+" Instances de "+str(plmnOR)+" trouvées dans "+str(ntmfile))
+print(str(bt)+" Instances de "+str(plmnBT)+" trouvées dans "+str(ntmfile))
+print(str(sfr)+" Instances de "+str(plmnSFR)+" trouvées dans "+str(ntmfile))
+
+file = open(ntmfile , 'r')
+file2 = open('20801.ntm' , 'w')
+file3 = open('20810.ntm' , 'w')
+file4 = open('20815.ntm' , 'w')
+file5 = open('20820.ntm' , 'w')
+for line in file.readlines():
+    if plmnOR in line:
+     file2.write(line)
+    elif plmnFree in line:
+     file4.write(line)
+    elif plmnBT in line:
+     file5.write(line)
+    elif plmnSFR in line:
+     file3.write(line)
+
+file.close()
+file2.close()
+file3.close()
+file4.close()
+file5.close()
+
